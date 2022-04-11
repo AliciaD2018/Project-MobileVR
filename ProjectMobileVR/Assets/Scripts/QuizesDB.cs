b96f9c0d12266a1d735d2fc65c0ae9fadf00b90b
Josué Gerardo <44277078+josuegerardo96@users.noreply.github.com>
2022-04-10 18:32:39 -0600

    Se agregan más policias funcionales y se limpia el código

--- ProjectMobileVR/Assets/Scripts/QuizesDB.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class QuizesDB : MonoBehaviour
-{
-  [SerializeField] private List<Question> m_questionList = null;
-}
